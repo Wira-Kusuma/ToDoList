837964d699cecae4e6317d6715b0132b98c2d435
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-12-18 22:43:19 +0700

    prototype

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './index.css'
   return(
     <main>
       <Header />
+      <Add />
       <List />
     </main>
   )
@@ -14,11 +15,69 @@ import './index.css'
 function Header() {
   return <h1>Shopping list</h1>
 }
+
+function Add() {
+  const [name, setName]  = useState("");
+  const [quantity, setQuantity]  = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(!name || !quantity) alert("do not blank"); return;
+    const newItem = {name, quantity, checked:false, id:Date.now()}
+    console.log(newItem);
+    setName("")
+    setQuantity("")
+  }
+  return(
+    <form className='add' onSubmit={handleSubmit}>
+      <input type="text" placeholder='add' value={name} onChange={(e) => setName(e.target.value)}/>
+
+      <input type="number" placeholder='quantity' value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}/>
+      <button>add</button>
+    </form>
+  )
+}
+
+const shoppingItem= [
+  {
+    id:1,
+    name:"kentang",
+    quantity:2,
+    checked:false
+  },
+  {
+    id:2,
+    name:"ayam paha", 
+    quantity:1,
+    checked:false
+  },
+  {
+    id:3,
+    name:"darah ayam",
+    quantity:5,
+    checked:false
+  },
+]
 function List() {
   return (
-    <div>
-      <input type="checkbox"/>
-      <span>kentang 1</span>
+    <div className='list'>
+      <ul>
+        {shoppingItem.map((item) => (
+          <Item item={item} key={item.key} />
+        ))}
+      </ul>
     </div>
+  )
+}
+
+function Item({item}) {
+  return(
+    <li key={item.id}>
+      <input type="checkbox"/>
+      <span style={item.checked ? {textDecoration:"line-through"} : {} }>
+        {item.name} {item.quantity}
+      </span>
+      <button>&times;</button>
+    </li>
   )
 }
